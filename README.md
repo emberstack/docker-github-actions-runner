44249f4047a7191ea20ce9317eb32de4f265afeb
Romeo Dumitrescu <romeo@drc.ro>
2023-12-24 19:06:03 +0200

    Initial build

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# docker-github-actions-runner
+# GitHub Self-Hosted Runner for Docker
+
+[![Pipeline](
+[GitHub Self-Hosted Runner for Docker](https://github.com/emberstack/docker-github-actions-runner) is self-hosted agent that you can run in a container with Docker.
+
+[![Pipeline](https://github.com/emberstack/docker-github-actions-runner/actions/workflows/pipeline.yaml/badge.svg)](https://github.com/emberstack/docker-github-actions-runner/actions/workflows/pipeline.yaml)
+[![Release](https://img.shields.io/github/release/emberstack/docker-github-actions-runner.svg?style=flat-square)](https://github.com/emberstack/docker-github-actions-runner/releases/latest)
+[![Docker Image](https://img.shields.io/docker/image-size/emberstack/github-actions-runner/latest?style=flat-square)](https://hub.docker.com/r/emberstack/github-actions-runner)
+[![Docker Pulls](https://img.shields.io/docker/pulls/emberstack/github-actions-runner.svg?style=flat-square)](https://hub.docker.com/r/emberstack/github-actions-runner)
+[![license](https://img.shields.io/github/license/emberstack/docker-github-actions-runner.svg?style=flat-square)](LICENSE)
+
+> Supports `amd64`, `arm` and `arm64`
+
+## Runner Version
+
+This image will automatically pull and install the latest runner version at startup.
+
+### Support
+If you need help or found a bug, please feel free to open an issue on the [emberstack/docker-github-actions-runner](https://github.com/emberstack/docker-github-actions-runner) GitHub project.  
+
+## Deployment
+
+The runner can be deployed in Docker using either `docker run` or `docker compose`.
+
+#### Deployment in `docker`
+
+```
+docker run -d -e ORGANIZATION="<org name>" -e ACCESS_TOKEN="<access token>" -e REPO="<optional repo name>" emberstack/github-actions-runner
+```
---
+++ src/docker/start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+ARCHITECTURE="$(arch)"
+PLATFORM=$ARCHITECTURE
+if [[ $PLATFORM == x86_64 ]]; then
+  PLATFORM="linux-x64"
+elif [[ $PLATFORM == arm* ]]; then
+  PLATFORM="linux-arm"
+elif [[ $PLATFORM == aarch64 ]]; then
+  PLATFORM="linux-arm64"
+else 
+  echo 1>&2 "Unsupported architecture"
+  exit 1
+fi
+
+ORGANIZATION=$ORGANIZATION
+ACCESS_TOKEN=$ACCESS_TOKEN
+REPO=$REPO
+
+echo "Setup - Determining matching runner..."
+
+RUNNER_VERSION=`curl -s -i https://github.com/actions/runner/releases/latest | grep location:`
+RUNNER_VERSION=`echo $RUNNER_VERSION | sed 's#.*tag/v##'`
+RUNNER_VERSION=`echo $RUNNER_VERSION | sed 's/\r//'`
+
+curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-$PLATFORM-${RUNNER_VERSION}.tar.gz 
+tar xzf ./actions-runner-$PLATFORM-${RUNNER_VERSION}.tar.gz
+
+
+./bin/installdependencies.sh
+
+export RUNNER_ALLOW_RUNASROOT="1"
+
+echo "Setup - Getting registration token..."
+if [ -z "$REPO" ]; then
+  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+else
+  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+fi
+
+
+echo "Setup - Configuring runner..."
+if [ -z "$REPO" ]; then
+  ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+else
+  ./config.sh --url https://github.com/${ORGANIZATION}/${REPO} --token ${REG_TOKEN}
+fi
+
+cleanup() {
+    echo "Exit - Removing runner..."
+    ./config.sh remove --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
